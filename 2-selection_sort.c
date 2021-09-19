d86e550255722ccdad97f8a7773ce8e1696a71cf
jswini <jswinikoff@gmail.com>
2021-09-19 10:56:38 -0500

    first commit selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t min_index;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		{
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[min_index])
+				{
+					min_index = j;
+				}
+			}
+		}
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+		}
+		print_array(array, size);
+	}
+}
